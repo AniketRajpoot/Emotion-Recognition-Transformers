5183034c42a951dc4c42039af7f4a01f8484a467
Aniket Singh Rajpoot <52102674+AniketRajpoot@users.noreply.github.com>
2021-02-10 01:52:43 +0530

    Update Runner.py

--- Raw-Data/Runner.py
+++ Raw-Data/Runner.py
@@ -54,7 +54,6 @@ def save_object(obj, filename):
     with open(filename, 'wb') as output:  # Overwrites any existing file.
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
-
 def load_object(filename):
     with open(filename, 'rb') as input:  # Overwrites any existing file.
         dataset_reload = pickle.load(input)
